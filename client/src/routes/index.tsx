b770104dc928f3de2e33f8c3e6caec4745b9b25f
Esteban Abanto <esteban.abanto.2709@gmail.com>
2025-09-01 09:44:09 -0500

    chore(dependencies): add @types/node and update path aliases for cleaner imports

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,7 +1,7 @@
-import Home from "../pages/Home";
-import About from "../pages/About";
-import NotFound from "../pages/NotFound";
-import MainLayout from "../layouts/MainLayout";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+import NotFound from "@/pages/NotFound";
+import MainLayout from "@/layouts/MainLayout";
 
 const routes = [
   {
